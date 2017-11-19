faa523cc6f6f9d107331a7bfa8740eddde3ee4d6
jbrxk <jbrxk@delmar.umsl.edu>
2017-11-19 17:43:34 -0600

    Added code to filter user input and store only first 10 digits from it

--- phone.c
+++ phone.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "phone_fmt.h"
 
 int main()
 {
-	char argm[25];
+	int i = 0, j = 0;
+	char argm[200], tel_num[10];
 
+	printf("Please type your 10 digit phone number");
 	printf("Input: ");
 	scanf("%s", argm);
 
 	printf("%s\n", argm);
+
+	while(argm[i] != '\0') {
+	
+		if(isdigit(argm[i])) {
+			tel_num[j] = argm[i];
+			j++;
+			if(j == 10) {
+				break;
+			}
+		}
+		
+		i++;
+	}
+
+	for(i = 0; i < 10; i++) {
+		printf("%c", tel_num[i]);
+	}
+	printf("\n");
+
+
 	return 0;
 }
